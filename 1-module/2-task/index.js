6f775806b9e2a3d0860bf0b06b76c709cf09b64d
Natalia Karelina <karnata21@gmail.com>
2024-08-13 01:54:21 +0300

    validate name task

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,7 +10,13 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  let result = false;
+
+  if (name && (name.length >= 4) && !name.includes(' ')) {
+     result = true;
+  }
+
+  return result;
 }
 
 function sayHello() {

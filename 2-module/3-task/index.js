666b2ed62be27bbe576e27b46b6f56b5d51ca53f
Natalia Karelina <karnata21@gmail.com>
2024-08-17 20:37:43 +0300

    2-module

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,6 +1,19 @@
 let calculator = {
-  // ваш код
+  read,
+  sum,
+  mul,
 };
+
+function read(a, b) {
+  this.read.a = a
+  this.read.b = b  
+}
+function sum(){
+  return this.read.a + this.read.b  
+}
+function mul(){
+  return this.read.a * this.read.b  
+}
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
 window.calculator = calculator; // делает ваш калькулятор доступным глобально

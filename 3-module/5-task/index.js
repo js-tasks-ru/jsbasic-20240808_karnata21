e9c22139e8f8a6fabae4039e0c11660ba1368593
Natalia Karelina <karnata21@gmail.com>
2024-08-20 03:02:49 +0300

    3-module

--- 3-module/5-task/index.js
+++ 3-module/5-task/index.js
@@ -1,3 +1,13 @@
 function getMinMax(str) {
-  // ваш код...
+  let result = {};
+
+  const sortStr = str
+    .split(' ')
+    .filter(item => Number(item))
+    .sort((a, b) => { return a - b });
+
+  result.min = Number(sortStr.shift())
+  result.max = Number(sortStr.pop())
+
+  return result;
 }

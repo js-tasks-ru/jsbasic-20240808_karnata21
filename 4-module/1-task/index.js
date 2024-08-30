e3cbc2e8ef44c74e9f5dab38525708c3a1837fb7
Natalia Karelina <karnata21@gmail.com>
2024-08-30 22:54:25 +0300

    4-module

--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,12 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement('ul')
+  let  template= ''
+
+  friends.forEach(friend => template +=(`
+  <li> ${friend.firstName} ${friend.lastName } </li>`) )
+     
+  ul.innerHTML = template
+  document.body.insertAdjacentHTML("afterbegin",ul.innerHTML)
+  
+return ul
 }
--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,36 @@
 function highlight(table) {
-  // ваш код...
+  
+  const allTr = Array.from(table.querySelectorAll('tr'));
+  const statusTd = []
+  const genderTd = []
+  const ageTd = []
+
+  allTr.forEach(tr => statusTd.push(tr.lastElementChild))
+  allTr.forEach(tr => genderTd.push(tr.children[2]))
+  allTr.forEach(tr => ageTd.push(tr.children[1]))
+
+  statusTd.forEach((td) => {
+    if (td.hasAttribute('data-available')) {
+      if (td.getAttribute('data-available') == 'true') {
+        td.parentElement.classList.add('available')
+      } else {
+        td.parentElement.classList.add('unavailable')
+      }
+    } else td.parentElement.setAttribute('hidden', true)
+  })
+
+  genderTd.forEach((td) => {
+    if (td.textContent == 'm') {
+      td.parentElement.classList.add('male')
+    } else if (td.textContent == 'f') {
+      td.parentElement.classList.add('female')
+    }
+  })
+
+  ageTd.forEach((td) => {
+    if (Number(td.textContent) < 18) {
+      td.parentElement.style = "text-decoration: line-through"
+    }
+  })
+  
 }

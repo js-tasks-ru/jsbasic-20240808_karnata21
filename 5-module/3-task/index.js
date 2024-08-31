6dbc0b67de91d31ca2f6569e458f2a9c1c34901d
Natalia Karelina <karnata21@gmail.com>
2024-08-31 16:53:25 +0300

    4,5-module

--- 5-module/3-task/index.js
+++ 5-module/3-task/index.js
@@ -1,3 +1,40 @@
 function initCarousel() {
-  // ваш код...
+  const rightButton = document.querySelector('.carousel__arrow_right');
+  const leftButton = document.querySelector('.carousel__arrow_left');
+  const lenta = document.querySelector('.carousel__inner');
+  const lentaOffsetWidth = lenta.offsetWidth;
+  const slideQuantity = document.querySelectorAll('.carousel__slide').length;
+
+  let i = 0
+  leftButton.style.display = 'none'
+
+  rightButton.addEventListener('click', (event) => {
+    leftButton.style.display = ''
+    i++
+    if (i == slideQuantity - 1) {
+      rightButton.style.display = 'none'
+    }
+    if (i < slideQuantity) {
+
+      styleTransform(i)
+
+    }
+  })
+
+  leftButton.addEventListener('click', (event) => {
+    rightButton.style.display = ''
+    i--
+    if (i == 0) {
+      leftButton.style.display = 'none'
+    }
+    if (i >= 0) {
+      styleTransform(i)
+    }
+  })
+
+  function styleTransform(i) {
+    let len = i * lentaOffsetWidth
+    lenta.style.transform = `translateX(-${len}px)`
+  }
+
 }

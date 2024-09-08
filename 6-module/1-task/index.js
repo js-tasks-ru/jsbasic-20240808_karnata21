f3fb0f6f5a0d6853c020ed5319f6509d3d2fecc9
Natalia Karelina <karnata21@gmail.com>
2024-09-09 01:21:01 +0300

    6-module

--- 6-module/1-task/index.js
+++ 6-module/1-task/index.js
@@ -13,6 +13,53 @@
  *
  */
 export default class UserTable {
+  elem
+  rows
+
   constructor(rows) {
+
+    this.rows = rows 
+    this.elem = document.createElement('table')
+    this.createTable()
+    this.deletedRow()
+
   }
+
+  createTable() {
+    const template =
+  ` <thead>
+        <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Зарплата</th>
+            <th>Город</th>
+            <th></th>
+        </tr>
+    </thead>
+    <tbody>     
+        ${this.rows.map(value =>
+        `<tr>
+      ${tdConstructor(value)}
+        <td><button>X</button></td>
+        </tr>
+        `).join('')}                     
+    </tbody>
+   `
+    this.elem.innerHTML = template
+    
+  }
+
+  deletedRow() {
+    for (let button of this.elem.querySelectorAll('button')) 
+      button.addEventListener('click', (event) =>
+        event.target.closest('tr').remove())                
+  }
+}
+ 
+function tdConstructor(value) {
+  let td = []
+  for (let key in value) {
+    td.push(`<td>${value[key]}</td>`)
+  }
+  return td.join('')
 }
--- 6-module/3-task/index.js
+++ 6-module/3-task/index.js
@@ -1,7 +1,94 @@
 import createElement from '../../assets/lib/create-element.js';
 
 export default class Carousel {
+  slides
+  elem
   constructor(slides) {
     this.slides = slides;
+    this.elem = document.createElement('div')
+    this.createSlides()
+    this.addProduct()
+    this.initCarousel()
+  }
+  createSlides(){
+    
+    let template =
+    `<div class="carousel__arrow carousel__arrow_right">
+        <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+      </div>
+      <div class="carousel__arrow carousel__arrow_left">
+        <img src="/assets/images/icons/angle-left-icon.svg" alt="icon">
+      </div>
+
+      <div class="carousel__inner">
+
+        ${this.slides.map(item =>
+        `<div class="carousel__slide" data-id="${item.id}">
+        <img src="/assets/images/carousel/${item.image}" class="carousel__img" alt="slide">
+        <div class="carousel__caption">
+          <span class="carousel__price">€${item.price.toFixed(2)}</span>
+          <div class="carousel__title">${item.name}</div>
+
+          <button type="button" class="carousel__button">
+            <img src="/assets/images/icons/plus-icon.svg" alt="icon">
+          </button>
+        </div>
+      </div>
+        `).join('')}                
+    </div>`
+
+     this.elem.classList.add("carousel")
+     this.elem.innerHTML = template
+  }
+  addProduct(){
+    let buttonsPlus = this.elem.querySelectorAll('.carousel__button')
+       buttonsPlus.forEach(buttonPlus => 
+      buttonPlus.addEventListener('click', (event) =>{
+
+      let slideId = buttonPlus.closest("[data-id]").dataset.id
+      buttonPlus.dispatchEvent(new CustomEvent("product-add", {
+        detail: slideId,
+        bubbles: true 
+      }))
+    }) 
+    )               
+  }
+  initCarousel() {
+    const rightButton = this.elem.querySelector('.carousel__arrow_right');
+    const leftButton = this.elem.querySelector('.carousel__arrow_left');
+    const lenta = this.elem.querySelector('.carousel__inner');
+    const lentaOffsetWidth = 500 // this.elem.querySelectorAll('.carousel__slide').offsetWidth;
+    const slideQuantity = this.elem.querySelectorAll('.carousel__slide').length;
+  
+    let i = 0
+    leftButton.style.display = 'none'
+  
+    rightButton.addEventListener('click', (event) => {
+      leftButton.style.display = ''
+      i++
+      if (i == slideQuantity - 1) {
+        rightButton.style.display = 'none'
+      }
+      if (i < slideQuantity) {
+  
+        let len = i * lentaOffsetWidth
+        lenta.style.transform = `translateX(-${len}px)`
+  
+      }
+    })
+  
+    leftButton.addEventListener('click', (event) => {
+      rightButton.style.display = ''
+      i--
+      if (i == 0) {
+        leftButton.style.display = 'none'
+      }
+      if (i >= 0) {
+        let len = i * lentaOffsetWidth
+        lenta.style.transform = `translateX(-${len}px)`
+      }
+    })
+
+  
   }
 }

8e827bc5f1420d9f4937dac08039c37c77eaa679
Natalia Karelina <karnata21@gmail.com>
2024-09-12 19:10:25 +0300

    2task module7

--- 7-module/1-task/index.js
+++ 7-module/1-task/index.js
@@ -2,6 +2,98 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class RibbonMenu {
   constructor(categories) {
-    this.categories = categories;
+    this.categories = categories
+    this.elem = this.render()
+    this.initMenu()
+    this.getCategory()
   }
+  template(){
+    let template =
+  `<div class="ribbon">
+   <button class="ribbon__arrow ribbon__arrow_left ribbon__arrow_visible">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </button>
+
+    <nav class="ribbon__inner">
+      <a href="#" class="ribbon__item ribbon__item_active" data-id="">All</a>
+      ${this.categories.slice(1).map(item =>
+      `<a href="#" class="ribbon__item" data-id="${item.id}">${item.name}</a>`).join('')}
+    </nav>
+
+    <button class="ribbon__arrow ribbon__arrow_right">
+      <img src="/assets/images/icons/angle-icon.svg" alt="icon">
+    </button>
+    </div>`
+    return template   
+  }
+  render(){
+    this.elem = createElement(this.template());
+
+    return this.elem
+  }
+  initMenu(){
+    const ribbonRight = this.elem.querySelector('.ribbon__arrow_right');
+    const ribbonLeft = this.elem.querySelector('.ribbon__arrow_left');
+    const ribbonInner = this.elem.querySelector('.ribbon__inner');
+  
+    
+    ribbonRight.classList.add("ribbon__arrow_visible")
+    ribbonLeft.classList.remove("ribbon__arrow_visible")
+  
+    ribbonRight.addEventListener('click', (event) => {
+   
+      let scrollRight = this.getScrollLenght(ribbonInner)
+  
+    ribbonInner.scrollBy(350, 0)
+
+    if (scrollRight < 1){
+      ribbonRight.classList.remove("ribbon__arrow_visible")
+     }else {
+      ribbonRight.classList.add("ribbon__arrow_visible")
+      ribbonLeft.classList.add("ribbon__arrow_visible")
+     }     
+    })
+
+    ribbonLeft.addEventListener('click', (event) => {
+   
+      let scrollLeft = ribbonInner.scrollLeft
+  
+     ribbonInner.scrollBy(-350, 0)
+
+    if (scrollLeft < 1){
+      ribbonLeft.classList.remove("ribbon__arrow_visible")
+     }else {
+      ribbonRight.classList.add("ribbon__arrow_visible")
+      ribbonLeft.classList.add("ribbon__arrow_visible")
+     }    
+    })
+  }
+  
+  getScrollLenght(ribbonInner){
+      let scrollLeft = ribbonInner.scrollLeft
+      let scrollWidth = ribbonInner.scrollWidth
+      let clientWidth = ribbonInner.clientWidth
+      let scrollRight = scrollWidth - scrollLeft - clientWidth
+
+      return scrollRight
+  }
+
+  getCategory(){
+    let addCategories = this.elem.querySelectorAll('.ribbon__item')
+    addCategories.forEach(categ => 
+      categ.addEventListener('click', (event) =>{
+        event.preventDefault()
+
+        categ.classList.add('ribbon__item_active')
+
+        let categId = categ.closest("[data-id]").dataset.id
+        categ.dispatchEvent(new CustomEvent("ribbon-select", {
+            detail: categId,
+            bubbles: true 
+   }))
+   
+ }) 
+ ) 
+  }
+  
 }
--- 7-module/2-task/index.js
+++ 7-module/2-task/index.js
@@ -2,5 +2,65 @@ import createElement from '../../assets/lib/create-element.js';
 
 export default class Modal {
   constructor() {
+    this.elem = createElement(this.template())
+  }
+  setTitle(text) {
+    const title = this.elem.querySelector('.modal__title')
+    title.textContent = text
+
+  }
+  setBody(mbody) {
+    const modalBody = this.elem.querySelector('.modal__body')
+    modalBody.innerHTML = ''
+    modalBody.append(mbody)
+  }
+
+  template() {
+    let template =
+      `<div class='modal'>
+    <div class="modal__overlay"></div>
+     <div class="modal__inner">
+       <div class="modal__header">  
+         <button type="button" class="modal__close">
+           <img src="/assets/images/icons/cross-icon.svg" alt="close-icon" />
+         </button>
+         <h3 class="modal__title">
+          Вот сюда нужно добавлять заголовок
+         </h3>
+       </div> 
+       <div class="modal__body">
+         A сюда нужно добавлять содержимое тела модального окна
+       </div>
+       </div>
+     </div>`
+
+    return template
+  }
+  open(){
+    let body = document.body
+    body.classList.add('is-modal-open')
+    body.append(this.elem)
+
+    
+    const modalClose = this.elem.querySelector('.modal__close')
+
+    const clickEsc = function (event) {
+      if (event.code === 'Escape' || (modalClose && event.target.closest('.modal__close'))) {
+       // body.classList.remove('is-modal-open')
+       // body.querySelector('.modal').remove()
+       this.Cclose()
+        document.removeEventListener('keydown', clickEsc)
+      }
+    }
+    modalClose.addEventListener('click', clickEsc)
+    document.addEventListener('keydown', clickEsc)
+
+
+  }
+  Cclose(){    
+   let body  = document.body
+        body.classList.remove('is-modal-open')
+        body.querySelector('.modal').remove()
+    
   }
 }
